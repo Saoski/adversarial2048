d715addeb33405a201917c367765a6e3a738ab7e
saoski <saoski88@gmail.com>
2026-03-19 10:34:20 -0400

    Save progross

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 from game import TwentyFortyEight, Direction
+import pygame as pg
+import pygame_gui as gui
+from components import game_cell
 
+# Quickstart guide for Pygame GUI: https://pygame-gui.readthedocs.io/en/latest/quick_start.html#quick-start-guides
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_DIMS = (WINDOW_WIDTH, WINDOW_HEIGHT)
+TARGET_FRAME_RATE = 60
 
-def game_loop(game: TwentyFortyEight) -> None:
-    while not game.is_game_over():
-        print(str(game))
-        direction: str = input("> ")
-        direction_enum: Direction = Direction.DOWN # Default to suppress warnings
-        match direction:
-            case "u":
-                direction_enum = Direction.UP
-            case "d":
-                direction_enum = Direction.DOWN
-            case "l":
-                direction_enum = Direction.LEFT
-            case "r":
-                direction_enum = Direction.RIGHT
-            case _:
-                return
-        state_changed = game.tilt(direction_enum)
-        if state_changed:
-            game.generate_new_tile()
-
+def draw_game(manager: gui.UIManager):
+    pass
 
 def main() -> None:
+    pg.init()
+
+    # Initialize game state
     game: TwentyFortyEight = TwentyFortyEight()
-    game_loop(game)
+
+    window_surface = pg.display.set_mode(WINDOW_DIMS)
+    clock = pg.time.Clock()
+    background = pg.Surface((800, 600))
+    background.fill(pg.Color("#000000"))
+
+    # Manages update, draw and event handling functions of all the UI elements
+    manager = gui.UIManager(WINDOW_DIMS)
+
+    running = True
+    while running:
+        time_delta = clock.tick(TARGET_FRAME_RATE) / 1000.0
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+            # Pass events to UI elements
+            manager.process_events(event)
+
+        # update what element is currently hovered
+        manager.update(time_delta)
+
+        # fill the screen with a color to wipe away anything from last frame
+        window_surface.blit(background, (0, 0))
+        manager.draw_ui(window_surface)
+
+        pg.display.update()
+
+    pg.quit()
 
 
 if __name__ == "__main__":
