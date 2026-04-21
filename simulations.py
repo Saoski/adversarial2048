0e6edf5edaf650bca80b68f0c305c39d25ae3237
saoski <saoski88@gmail.com>
2026-04-21 02:39:10 -0400

    Fix my sims

--- simulations.py
+++ simulations.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from game import TwentyFortyEight, Direction
 from models import min_max_play, random_play
-from models import random_play, expectimax, compute_direction
+from models import random_play, expectimax, compute_direction, monte_carlo
 from time import perf_counter
 from typing import Any
 from concurrent.futures import ProcessPoolExecutor
@@ -16,11 +16,13 @@ class GameStats:
     largest_tile: int
 
 
-def run_minimax_vs_monte_carlo(count: int, depth: int, rollout: int) -> list[GameStats]:
+def run_minimax_vs_monte_carlo(
+    count: int, minimax_depth: int, rollout: int, monte_carlo_depth: int
+) -> list[GameStats]:
     player_fn = min_max_play
     player_options = dict()
     player_options["is_player_one"] = True
-    player_options["depth"] = depth
+    player_options["depth"] = minimax_depth
     player_options["new_tile_min"] = True
     player_options["player_one_min"] = False
     player_options["player_two_min"] = True
@@ -28,41 +30,48 @@ def run_minimax_vs_monte_carlo(count: int, depth: int, rollout: int) -> list[Gam
     adversary_fn = monte_carlo
     adversary_options = dict()
     adversary_options["is_player"] = False
-    adversary_options["depth"] = depth
+    adversary_options["depth"] = monte_carlo_depth
     adversary_options["rollouts"] = rollout
 
     return simulate(player_fn, player_options, adversary_fn, adversary_options, count)
 
-def run_expectimax_vs_monte_carlo(count: int, depth: int, rollout: int) -> list[GameStats]:
+
+def run_expectimax_vs_monte_carlo(
+    count: int, expectimax_depth: int, rollout: int, monte_carlo_depth: int
+) -> list[GameStats]:
     player_fn = expectimax
     player_options = dict()
     player_options["is_player"] = True
-    player_options["depth"] = depth
+    player_options["depth"] = expectimax_depth
 
     adversary_fn = monte_carlo
     adversary_options = dict()
     adversary_options["is_player"] = False
-    adversary_options["depth"] = depth
+    adversary_options["depth"] = monte_carlo_depth
     adversary_options["rollouts"] = rollout
 
     return simulate(player_fn, player_options, adversary_fn, adversary_options, count)
 
-def run_monte_carlo_vs_minimax(count: int, depth: int, rollout: int) -> list[GameStats]:
+
+def run_monte_carlo_vs_minimax(
+    count: int, minimax_depth: int, rollout: int, monte_carlo_depth: int
+) -> list[GameStats]:
     player_fn = monte_carlo
     player_options = dict()
     player_options["is_player"] = True
-    player_options["depth"] = depth
+    player_options["depth"] = monte_carlo_depth
     player_options["rollouts"] = rollout
 
     adversary_fn = min_max_play
     adversary_options = dict()
     adversary_options["is_player_one"] = False
-    adversary_options["depth"] = depth
+    adversary_options["depth"] = minimax_depth
     adversary_options["new_tile_min"] = False
     adversary_options["player_one_min"] = False
     adversary_options["player_two_min"] = True
 
     return simulate(player_fn, player_options, adversary_fn, adversary_options, count)
+
 
 def run_minimax_vs_random(count: int, depth: int) -> list[GameStats]:
     player_fn = min_max_play
